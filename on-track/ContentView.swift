568a0010d311e0b944e7fdf7c0e27bb162d6ac0a
Alexey Ankip <aliaksei.ankip@jobtome.com>
2022-08-12 15:16:46 +0400

    Initial Commit

---
+++ on-track/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  on-track
+//
+//  Created by Alexey Ankip on 12.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ on-track/on_trackApp.swift
@@ -0,0 +1,17 @@
+//
+//  on_trackApp.swift
+//  on-track
+//
+//  Created by Alexey Ankip on 12.08.22.
+//
+
+import SwiftUI
+
+@main
+struct on_trackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
